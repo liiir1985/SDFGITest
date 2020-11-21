0b08c807913d7ec70c2757f24a124b65eca92556
liiir1985 <liiir1985@hotmail.com>
2020-11-21 14:45:16 +0800

    绘制BVH节点

---
+++ unity/SDF Generator/Assets/Signed Distance Field Generator/Scripts/Editor/SDFGIEditor.cs
@@ -0,0 +1,27 @@
+﻿//
+// Copyright © Daniel Shervheim, 2019
+// www.danielshervheim.com
+//
+
+using UnityEngine;
+using UnityEditor;
+
+using Unity.Collections;
+using Unity.Jobs;
+using Unity.Mathematics;
+namespace SDFGenerator
+{
+    [CustomEditor(typeof(SDFGI))]
+    public class SDFGIEditor : Editor
+    {
+        int curDepth;
+        public unsafe override void OnInspectorGUI()
+        {
+            SDFGI sdfgi = target as SDFGI;
+            int maxDepth = sdfgi.MaxDepth;
+
+            curDepth = EditorGUILayout.IntSlider("Draw Depth", curDepth, 0, maxDepth);
+            sdfgi.GizmoDepth = curDepth;
+        }
+    }
+}
--- unity/SDF Generator/Assets/Signed Distance Field Generator/Scripts/SDFGI.cs
+++ unity/SDF Generator/Assets/Signed Distance Field Generator/Scripts/SDFGI.cs
@@ -21,5 +21,57 @@ namespace SDFGenerator
             bvh = new BVH(volumes);
             bvh.Build();
         }
+#if UNITY_EDITOR
+        public int GizmoDepth { get; set; }
+        int maxDepth = -1;
+        public int MaxDepth => maxDepth;
+        void CalcDepth(BVHNode node, ref int depth)
+        {
+            int leftDepth = depth + 1;
+            int rightDepth = depth + 1;
+
+            if (node.Left != null)
+                CalcDepth(node.Left, ref leftDepth);
+            else
+                leftDepth = depth;
+
+            if (node.Right != null)
+                CalcDepth(node.Right, ref rightDepth);
+            else
+                rightDepth = depth;
+
+            depth = Mathf.Max(leftDepth, rightDepth);
+        }
+
+        void DrawBVHNode(BVHNode node, int depth)
+        {
+            if(depth == GizmoDepth)
+            {
+                Gizmos.color = Color.yellow;
+                Gizmos.DrawWireCube(node.AABB.center, node.AABB.size);
+            }
+            else if(depth < GizmoDepth)
+            {
+                if (node.Left != null)
+                    DrawBVHNode(node.Left, depth + 1);
+                if (node.Right != null)
+                    DrawBVHNode(node.Right, depth + 1);
+            }
+        }
+        private void OnDrawGizmosSelected()
+        {
+            if (bvh != null)
+            {
+                if (maxDepth < 0)
+                {
+                    int depth = 0;
+                    CalcDepth(bvh.Root, ref depth);
+                    maxDepth = depth;
+                }
+
+                DrawBVHNode(bvh.Root, 0);
+            }
+        }
+#endif
     }
 }
